f47ba45b1502617a97d6495c0f745f8b6bc5345e
Eatmeta <metagroul@gmail.com>
2022-11-11 16:51:59 +0300

    Implement a moving average method MovingAverage

--- MovingAverageTask.cs
+++ MovingAverageTask.cs
@@ -2,12 +2,37 @@ using System.Collections.Generic;
 
 namespace yield
 {
-	public static class MovingAverageTask
-	{
-		public static IEnumerable<DataPoint> MovingAverage(this IEnumerable<DataPoint> data, int windowWidth)
-		{
-			//Fix me!
-			return data;
-		}
-	}
+    public static class MovingAverageTask
+    {
+        public static IEnumerable<DataPoint> MovingAverage(this IEnumerable<DataPoint> data, int windowWidth)
+        {
+            var isFirst = true;
+            var queue = new Queue<double>();
+            var sumWindowElements = 0.0;
+
+            foreach (var dataPoint in data)
+            {
+                if (isFirst || windowWidth == 1)
+                {
+                    isFirst = false;
+                    queue.Enqueue(dataPoint.OriginalY);
+                    sumWindowElements += dataPoint.OriginalY;
+                    yield return dataPoint.WithAvgSmoothedY(dataPoint.OriginalY);
+                }
+                else if (queue.Count < windowWidth)
+                {
+                    sumWindowElements += dataPoint.OriginalY;
+                    queue.Enqueue(dataPoint.OriginalY);
+                    yield return dataPoint.WithAvgSmoothedY(sumWindowElements / queue.Count);
+                }
+                else
+                {
+                    sumWindowElements -= queue.Dequeue();
+                    sumWindowElements += dataPoint.OriginalY;
+                    queue.Enqueue(dataPoint.OriginalY);
+                    yield return dataPoint.WithAvgSmoothedY(sumWindowElements / windowWidth);
+                }
+            }
+        }
+    }
 }
