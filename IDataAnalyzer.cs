f4c01d2a4afff8d2ed38c38c4a5ea3fa11cf4282
Eatmeta <metagroul@gmail.com>
2022-11-11 16:46:39 +0300

    Initial commit

---
+++ IDataAnalyzer.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+
+namespace yield
+{
+	public interface IDataAnalyzer
+	{
+		IEnumerable<DataPoint> SmoothExponentially(IEnumerable<DataPoint> data, double alpha);
+		IEnumerable<DataPoint> MovingAverage(IEnumerable<DataPoint> data, int windowWidth);
+		IEnumerable<DataPoint> MovingMax(IEnumerable<DataPoint> data, int windowWidth);
+	}
+
+	public class DataAnalyzerImpl : IDataAnalyzer
+	{
+		public IEnumerable<DataPoint> SmoothExponentially(IEnumerable<DataPoint> data, double alpha)
+		{
+			return data.SmoothExponentially(alpha);
+		}
+
+		public IEnumerable<DataPoint> MovingAverage(IEnumerable<DataPoint> data, int windowWidth)
+		{
+			return data.MovingAverage(windowWidth);
+		}
+
+		public IEnumerable<DataPoint> MovingMax(IEnumerable<DataPoint> data, int windowWidth)
+		{
+			return data.MovingMax(windowWidth);
+		}
+	}
+}
